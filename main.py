a8fa8477aaa1e34d0bed1a67ef90fe091f84f9bb
meekachm <meeka.chm@gmail.com>
2023-12-04 21:23:25 -0800

    Challenge 2: Created Order class with some attributes and a method

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ class Person:
 
     def my_order(self):
 
+class Order:
+    def __init__(self, person, drink_type):
+        self.person = person
+        self.drink_type = drink_type
 
+    def to_string(self):
+        return f"{self.person.name} orders: {self.drink_type}"
 
 
